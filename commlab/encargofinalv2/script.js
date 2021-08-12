316af3b5c0c24f775b5c081bf843a79f883082e1
jorrrge04 <jorgestebangonzalez31@gmail.com>
2021-08-12 18:08:15 -0400

    encargofinalv2

---
+++ commlab/encargofinalv2/script.js
@@ -0,0 +1,18 @@
+document.getElementById('imagen1').addEventListener('mouseenter', ()=> {
+  document.getElementById('imagen1').src = "torredecontrolcolor.png";
+})
+document.getElementById('imagen1').addEventListener('mouseleave', ()=> {
+  document.getElementById('imagen1').src = "torredecontrol.png";
+})
+document.getElementById('imagen2').addEventListener('mouseenter', ()=> {
+  document.getElementById('imagen2').src = "balcondevigilanciacolor.png";
+})
+document.getElementById('imagen2').addEventListener('mouseleave', ()=> {
+  document.getElementById('imagen2').src = "balcondevigilancia.png";
+})
+document.getElementById('imagen3').addEventListener('mouseenter', ()=> {
+  document.getElementById('imagen3').src = "tallerdenavescolor.png";
+})
+document.getElementById('imagen3').addEventListener('mouseleave', ()=> {
+  document.getElementById('imagen3').src = "tallerdenaves.png";
+})
